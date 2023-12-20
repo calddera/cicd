5e469157780829d7a832b8f88a8735c063ae9843
calddera <calddera@gmail.com>
2023-12-20 10:15:22 -0600

    Probando con merge 3

--- app/Http/Controllers/Api/V1/EmployeeController.php
+++ app/Http/Controllers/Api/V1/EmployeeController.php
@@ -29,11 +29,6 @@ class EmployeeController extends Controller
             ]
         )->get();
 
-        $employees = [
-            "hola" => "mundo",
-            "Cara" => "Colas"
-        ];
-
         return ApiResponse::success($employees);
     }
 
